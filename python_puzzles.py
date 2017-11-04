c63f118c4b3d3ba16b251acb94dfe2465818b89c
Tom Archer <tomm.archer@gmail.com>
2017-11-04 22:32:07 +0000

    Add menu and data display

--- python_puzzles.py
+++ python_puzzles.py
@@ -1,12 +1,10 @@
 import argparse
+import curses
 import pi3d
 import random
 import time
 from image_randomiser import ImageRandomiser
 from status_display import StatusDisplay
-
-from PyQt4 import QtGui
-from PyQt4 import QtCore
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description='Run Python Puzzles Masterclass Finalé.')
@@ -19,11 +17,6 @@ LOOP_TIME  = 10
 IMAGE_SIZE = 900
 PIXEL_SIZE = 5
 BACKGROUND_COLOR = (0.3, 0.3, 0.3, 1.0)
-
-print("""
-Space to start/transition
-ESC to quit
-""")
     
 KEYBOARD = pi3d.Keyboard()
 LOGGER = pi3d.Log(__name__, level='INFO', file='info.log')
@@ -34,11 +27,17 @@ CAMERA = pi3d.Camera(at=(HWIDTH,-HHEIGHT,0), eye=(HWIDTH,-HHEIGHT,-0.1), is_3d=F
 
 points = ImageRandomiser("MasterclassImage960.png", IMAGE_SIZE, IMAGE_SIZE, PIXEL_SIZE, CAMERA)
 
-def finale_mode():
+def finale_mode(screen):
     try:
         dummy_mode = True
         while dummy_mode:
+
             # parse teams and requests from server
+            data = ["22:04:22 - data received from team 1 (192.168.0.2) is sorted",
+                    "22:04:47 - data received from team 2 (192.168.0.3) is NOT sorted",
+                    "22:04:53 - data received from team 3 (192.168.0.4) is sorted"]
+            draw_curses_data(screen, data) 
+            screen.refresh()    
 
             # Parse keypresses
             k = KEYBOARD.read()
@@ -132,10 +131,45 @@ def demo_mode():
             DISPLAY.stop()
             LOGGER.info(str(e))    
 
+def draw_curses_menu(screen):
+    screen.border(0)
+    screen.addstr(2, 2, "Dummy server is currently running.")
+    screen.addstr(4, 2, "Please select an option...")
+    screen.addstr(6, 4, "ESC   - Exit")
+    screen.addstr(7, 4, "SPACE - Start finalé")
 
+    screen.move(9, 0)
+    screen.addch(curses.ACS_LTEE)
+    height, width = screen.getmaxyx()
+    screen.hline(curses.ACS_HLINE, width)
+    screen.move(9, width-1)
+    screen.addch(curses.ACS_RTEE)
+
+def draw_curses_data(screen, data):
+    # Clear previous rows
+    height, width = screen.getmaxyx()
+    for i in range(11, 21):
+        if i < height-2:
+            screen.move(i, 2)
+            screen.hline(" ", width-3)    
+
+    # Show new rows
+    i=11
+    for data_line in data:
+        if i < height-2:
+            screen.addstr(i, 2, data_line)
+            if len(data_line) > width-2:
+                i = i + 2
+            else:
+                i = i + 1
+         
 if args.demo:
     LOGGER.info('Running in Demo Mode')
     demo_mode()
 else:
+    screen = KEYBOARD.key
+    draw_curses_menu(screen)
+    screen.refresh()    
+    
     LOGGER.info('Running in Finalé Mode')
-    finale_mode()
+    finale_mode(screen)
